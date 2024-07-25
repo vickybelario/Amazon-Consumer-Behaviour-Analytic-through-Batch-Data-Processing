8b9177c6e2a60aa77d95da4fda000e3b7fbc5d30
vickybelario <vickybelario@gmail.com>
2024-07-25 11:51:00 +0700

    commitamazon

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Amazon-Customer-Behaviour-Analytics
+
+Introduction
+Nama  : Vicky Belario
+
+Project ini dilakukan untuk  membuat report  yang berisi Exploratory Data Analysis (EDA) dari Amazon consumer Behaviour Dataset
+
+Objective
+- Memahami konsep konsep NoSQL.
+- Dapat mempersiapkan data untuk digunakan dalam database NoSQL.
+- Mampu menggunakan Apache Airflow.
+- Bisa menerapkan mengolah dan memvisualisasikan data dengan menggunakan Kibana.
+- Sanggup melakukan validasi data dengan menggunakan Great Expectations.
+
+Background
+
+Dalam era digital saat ini, memahami perilaku konsumen sangat penting bagi bisnis untuk berkembang dan tetap kompetitif. Amazon, sebagai salah satu platform e-commerce terbesar di dunia, memiliki basis pelanggan yang luas dan beragam. Menganalisis pola perilaku konsumen Amazon dapat memberikan wawasan tentang pola interaksi pengguna,  preferensi, dan proses pengambilan keputusan pelanggan Amazon. 
+
+Goals
+
+Tujuan utama dari proyek milestone ini adalah menganalisis perilaku konsumen Amazon. Analisis ini akan memberikan gambaran tentang demografi pelanggan, kebiasaan umum pengguna dalam menjelajah dan berbelanja di Amazon
--- amazon behaviour code/DAG.py
+++ amazon behaviour code/DAG.py
@@ -1,29 +1,3 @@
-"""
-Milestone 3
-
-Introduction
-- Nama  : Vicky Belario
-- Batch : 017 HCK
-
-Milestone 3 ini dilakukan untuk  membuat report  yang berisi Exploratory Data Analysis (EDA) dari Amazon consumer Behaviour Dataset
-
-Objective
-- Memahami konsep konsep NoSQL.
-- Dapat mempersiapkan data untuk digunakan dalam database NoSQL.
-- Mampu menggunakan Apache Airflow.
-- Bisa menerapkan mengolah dan memvisualisasikan data dengan menggunakan Kibana.
-- Sanggup melakukan validasi data dengan menggunakan Great Expectations.
-
-Background
-
-Dalam era digital saat ini, memahami perilaku konsumen sangat penting bagi bisnis untuk berkembang dan tetap kompetitif. Amazon, sebagai salah satu platform e-commerce terbesar di dunia, memiliki basis pelanggan yang luas dan beragam. Menganalisis pola perilaku konsumen Amazon dapat memberikan wawasan tentang pola interaksi pengguna,  preferensi, dan proses pengambilan keputusan pelanggan Amazon. 
-
-Goals
-
-Tujuan utama dari proyek milestone ini adalah menganalisis perilaku konsumen Amazon. Analisis ini akan memberikan gambaran tentang demografi pelanggan, kebiasaan umum pengguna dalam menjelajah dan berbelanja di Amazon
-
-"""
-
 from airflow.models import DAG
 from airflow.operators.python import PythonOperator
 
@@ -37,9 +11,9 @@ from elasticsearch import Elasticsearch
 #from airflow.providers.postgres.operators.postgres import PostgresOperator
 #from airflow.utils.task_group import TaskGroup
 
-database = "airflow_m3"
-username = "airflow_m3"
-password = "airflow_m3"
+database = "airflow"
+username = "airflow"
+password = "airflow"
 host = "postgres"
 
 # Membuat URL koneksi PostgreSQL
@@ -50,17 +24,17 @@ postgres_url = f"postgresql+psycopg2://{username}:{password}@{host}/{database}"
 def load_csv_to_postgres():
     '''
     Fungsi untuk memuat data dari file CSV ke dalam tabel PostgreSQL.
-    Membaca file CSV yang berada di '/opt/airflow/dags/P2M3_vicky_belario_data_raw.csv'
-    dan menyimpannya ke tabel 'table_m3' di database PostgreSQL.
+    Membaca file CSV yang berada di '/opt/airflow/dags/amazon_data_raw.csv'
+    dan menyimpannya ke tabel 'table' di database PostgreSQL.
     '''
     # menggunakan URL saat membuat koneksi SQLAlchemy
     engine = create_engine(postgres_url)
     # engine= create_engine("postgresql+psycopg2://airflow:airflow@postgres/airflow")
     conn = engine.connect()
 
-    df = pd.read_csv('/opt/airflow/dags/P2M3_vicky_belario_data_raw.csv')
+    df = pd.read_csv('/opt/airflow/dags/amazon_data_raw.csv')
     #df.to_sql(nama_table_db, conn, index=False, if_exists='replace')
-    df.to_sql('table_m3', conn, 
+    df.to_sql('table', conn, 
               index=False, 
               if_exists='replace')  # M
     
@@ -69,15 +43,15 @@ def load_csv_to_postgres():
 def ambil_data():
     ''' 
     Fungsi untuk mengambil data dari tabel PostgreSQL dan menyimpannya ke file CSV baru.
-    Membaca data dari tabel 'table_m3' di PostgreSQL dan menyimpannya ke 
-    '/opt/airflow/dags/P2M3_vicky_belario_data_new.csv'.
+    Membaca data dari tabel 'table' di PostgreSQL dan menyimpannya ke 
+    '/opt/airflow/dags/amazon_data_new.csv'.
     '''
     # membuat koneksi SQLAlchemy
     engine = create_engine(postgres_url)
     conn = engine.connect()
 
-    df = pd.read_sql_query('select * from table_m3', conn) #nama table sesuai dengan nama table di postgres
-    df.to_csv('/opt/airflow/dags/P2M3_vicky_belario_data_new.csv', sep=',', index=False)
+    df = pd.read_sql_query('select * from table', conn) #nama table sesuai dengan nama table di postgres
+    df.to_csv('/opt/airflow/dags/amazon_data_new.csv', sep=',', index=False)
     
 
 
@@ -85,10 +59,10 @@ def preprocessing():
     ''' 
     Fungsi untuk membersihkan data.
     Menghapus duplikat, memformat kolom 'Timestamp', dan mengubah nama kolom menjadi huruf kecil.
-    Menyimpan data yang sudah dibersihkan ke file CSV '/opt/airflow/dags/P2M3_vicky_belario_data_clean.csv'.
+    Menyimpan data yang sudah dibersihkan ke file CSV '/opt/airflow/dags/amazon_data_clean.csv'.
     '''
     # pembersihan data
-    df = pd.read_csv("/opt/airflow/dags/P2M3_vicky_belario_data_new.csv")
+    df = pd.read_csv("/opt/airflow/dags/amazon_data_new.csv")
     
     #df['Timestamp'] = df['Timestamp'].apply(parser.parse)
     # Convert timestamp to datetime
@@ -110,19 +84,19 @@ def preprocessing():
     df.dropna(inplace=True)     
     df.drop_duplicates(inplace=True)
 
-    df.to_csv('/opt/airflow/dags/P2M3_vicky_belario_data_clean.csv', index=False)
+    df.to_csv('/opt/airflow/dags/amazon_data_clean.csv', index=False)
 
 def upload_to_elasticsearch():
     '''
     Fungsi untuk mengunggah data yang telah dibersihkan ke Elasticsearch.
-    Membaca data dari file CSV '/opt/airflow/dags/P2M3_vicky_belario_data_clean.csv' dan mengunggahnya ke index 'table_test_m3' di Elasticsearch.
+    Membaca data dari file CSV '/opt/airflow/dags/amazon_data_clean.csv' dan mengunggahnya ke index 'table_test' di Elasticsearch.
     '''
     es = Elasticsearch("http://elasticsearch:9200")
-    df = pd.read_csv('/opt/airflow/dags/P2M3_vicky_belario_data_clean.csv')     
+    df = pd.read_csv('/opt/airflow/dags/amazon_data_clean.csv')     
     
     for i, r in df.iterrows():
         doc = r.to_dict() #Konversi baris menjadi dictionary
-        res = es.index(index="table_test_m3", id=i+1, body=doc)
+        res = es.index(index="table_test", id=i+1, body=doc)
         print(f"Response from Elasticsearch: {res}")
         
 
@@ -136,8 +110,8 @@ default_args = {
 
 
 with DAG(
-    "P2M3_P2M3_vicky_belario_DAG_hck", #atur nama project 
-    description='Milestone_3',
+    "DAG", #atur nama project 
+    description='amazon customer behaviour',
     schedule_interval='30 6 * * *', #atur schedule untuk menjalankan airflow pada 06:30.
     default_args=default_args, 
     catchup=False
